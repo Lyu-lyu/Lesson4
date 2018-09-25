530b190d30b6f3fdb6490e6e6f2879db8c3397fe
unknown <for_lyuda@inbox.ru>
2018-09-25 23:45:13 +0300

    new version

--- collecting_data.py
+++ collecting_data.py
@@ -1,7 +1,21 @@
 import json, csv
 from yahoofinancials import YahooFinancials
 
-yahoo_financials = YahooFinancials('MCD') #выбраны котировки McDonald's
-js_array = yahoo_financials.get_historical_price_data('1970-01-02', '2018-09-25', 'daily') #выбираем всю историю
 
 
+def make_csv_with_ticker_data(ticker):
+    yahoo_financials = YahooFinancials(ticker) #выбраны котировки 
+    day_ago = '2018-09-25'
+    js_array = yahoo_financials.get_historical_price_data('1900-01-02', day_ago, 'daily') #выбираем всю историю
+    prices_array = js_array[ticker]["prices"]
+
+    with open (ticker + '.csv', 'w', encoding = 'utf-8', newline = '') as file_data:
+        fields = ["formatted_date", "open", "close"]
+        writer = csv.DictWriter(file_data, fields, delimiter=';',extrasaction='ignore')
+        writer.writeheader()
+        for price_str in prices_array:
+            pass
+            writer.writerow(price_str)
+
+ticker = input('Введите тикер: ')
+make_csv_with_ticker_data(ticker)

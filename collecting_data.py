abf9e7310fce1ee8aa787ca86a2a70be71eaf877
unknown <for_lyuda@inbox.ru>
2018-09-28 08:04:44 +0300

    remake

--- collecting_data.py
+++ collecting_data.py
@@ -3,28 +3,34 @@ from yahoofinancials import YahooFinancials
 from datetime import date, timedelta
 
 
-def make_csv_with_ticker_data(ticker):
+def make_csv_with_ticker_data(ticker, today):
     yahoo_financials = YahooFinancials(ticker) #taking rates by company ticker  
-    yesterday = date.today() - timedelta(1)
-    day_ago = yesterday.strftime('%Y-%m-%d')
-    js_array = yahoo_financials.get_historical_price_data('1900-01-02', day_ago, 'daily') #getting all history data
-    prices_array = js_array[ticker]['prices']
-    
-    with open ('{}.csv'.format(ticker), 'w', encoding = 'utf-8', newline = '') as file_data:
+    js_array = yahoo_financials.get_historical_price_data('1900-01-02', today, 'daily') #getting all history data
+    price_array = js_array[ticker]['prices']
+    return price_array
+
+
+def save_to_file(ticker, spreadsheet):
+    filename = '{}.csv'.format(ticker)
+    with open (filename, 'w', encoding = 'utf-8', newline = '') as file_data:
         fields = ['formatted_date', 'open', 'close']
         writer = csv.DictWriter(file_data, fields, delimiter=';',extrasaction='ignore')
         writer.writeheader()
-        for price_str in prices_array:
+        for price_str in spreadsheet:
             writer.writerow(price_str)
 
 
-while True:
-    ticker = input('Введите тикер: ').upper()
-    try: 
-        make_csv_with_ticker_data(ticker)
-        print('Файл успешно создан')
-        break
-    except ValueError:
-        print('Введены неверные данные!')
+def main():
+    while True:
+        ticker = input('Введите тикер: ').upper()
+        today = date.today().strftime('%Y-%m-%d')
+        try:
+            save_to_file(ticker, make_csv_with_ticker_data(ticker, today))
+            print('Файл успешно создан')
+            break
+        except ValueError:
+            print('Введены неверные данные!')
 
-make_csv_with_ticker_data(ticker)
+
+if __name__ == '__main__':
+    main()
